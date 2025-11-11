ae992f0bf358e6c6165a42c62ef1555f1e93c870
Chiara Richiardi <53477976+chiararik@users.noreply.github.com>
2025-11-11 09:58:07 +0100

    Change Python command from 'python' to 'python3'

--- src/R/SUHI_mapping.R
+++ src/R/SUHI_mapping.R
@@ -618,7 +618,7 @@ username = 'chiara.richiardi'
 system('pip install requests pandas')
 
 command = paste0(
-  "python downloader.py",
+  "python3 downloader.py",
   " --token ", token,
   " --username ", username,
   " --bbox ", aoi_bb['x', 'min'], " ", aoi_bb['y', 'min'], 
